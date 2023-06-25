cf7c97ac9b8bb304c75a406167c2d61c193b4eef
Prince Bioh <princebioh15@gmail.com>
2023-06-25 03:41:21 +0000

    Auth Service Add

---
+++ lib/auth/auth.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Authenticate {
+  // Initialize FIREBASE AUTH
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  // Create Method for Sign In
+  Future<void> emailSignIn(
+      {required String email, required String password}) async {
+    try {
+      await _auth.signInWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      // TODO: Pass Error Message to Alert Dialog
+      print(e.code);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
+  // Create Method for Sign Up
+  Future<void> emailSignUp({
+    required String email,
+    required String password,
+  }) async {
+    try {
+      await _auth.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+    } on FirebaseAuthException catch (e) {
+      // TODO: Pass Error Message to Alert Dialog
+      print(e.code);
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
