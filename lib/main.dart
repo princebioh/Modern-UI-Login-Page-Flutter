1b5ef30efd30bddba1df9a76713b4abada121554
Prince Bioh <princebioh15@gmail.com>
2023-06-25 03:24:07 +0000

    Initialize Fb

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:login_ui_page/firebase_options.dart';
 import 'package:login_ui_page/routes/routes.dart';
 
-
-void main(List<String> args) {
+void main(List<String> args) async {
+  WidgetsFlutterBinding.ensureInitialized;
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const MyApp());
 }
 
